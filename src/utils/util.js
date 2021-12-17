a97e186e74beb6faf7d67aa8db6c2a3b5d85168b
amber871023 <60221403+amber871023@users.noreply.github.com>
2021-12-17 17:52:21 +0800

    header驗證

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,14 +1,15 @@
-//jsSHA
-function getAuthorizationHeader() {
-  //  填入自己 ID、KEY 開始
-      let AppID = '63a8d7db95214e20bac7d3872a6371a4';
-      let AppKey = 'nb1XKPLjlA2Lp7W3-o1PYNuu87E';
-  //  填入自己 ID、KEY 結束
-      let GMTString = new Date().toGMTString();
-      let ShaObj = new jsSHA('SHA-1', 'TEXT');
-      ShaObj.setHMACKey(AppKey, 'TEXT');
-      ShaObj.update('x-date: ' + GMTString);
-      let HMAC = ShaObj.getHMAC('B64');
-      let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
-      return { 'Authorization': Authorization, 'X-Date': GMTString }; 
-  }
+import JsSHA from 'jssha'
+// jsSHA
+export default function getAuthorizationHeader () {
+  // 填入自己 ID、KEY 開始
+  const AppID = '63a8d7db95214e20bac7d3872a6371a4'
+  const AppKey = 'nb1XKPLjlA2Lp7W3-o1PYNuu87E'
+  // 填入自己 ID、KEY 結束
+  const GMTString = new Date().toGMTString()
+  const ShaObj = new JsSHA('SHA-1', 'TEXT')
+  ShaObj.setHMACKey(AppKey, 'TEXT')
+  ShaObj.update('x-date: ' + GMTString)
+  const HMAC = ShaObj.getHMAC('B64')
+  const Authorization = 'hmac username="' + AppID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"'
+  return { Authorization: Authorization, 'X-Date': GMTString }
+}
