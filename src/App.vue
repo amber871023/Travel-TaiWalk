0da880628c03b43cbeecf202c112e8241bf4f902
amber871023 <60221403+amber871023@users.noreply.github.com>
2021-12-17 17:48:15 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <button type="button" class="btn btn-primary">Primary</button>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
-
-<style scoped lang="scss">
-@import './assets/stylesheets/all';
-</style>
