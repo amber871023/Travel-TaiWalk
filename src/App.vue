597a3cbc2204b28c2c423adda419fa5dd6e2c696
amber871023 <60221403+amber871023@users.noreply.github.com>
2021-12-26 16:04:05 +0800

    切版,API,完成部分功能

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <router-view />
+    <router-view></router-view>
 </template>
+<style lang="scss">
+// app css
+#app {
+  font-family: 'Noto Sans TC', 'Playfair Display', sans-serif;
+}
+</style>
